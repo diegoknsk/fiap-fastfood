4ecdd020d309d5b5710644445c79320cdf4b5db4
Diego <diego.sa@pricefy.com.br>
2025-05-25 22:23:01 -0300

    ajustando repositorio para consultar o banco todo, subir todas apis olhando para o banco criar as politicas pela app service ou serrvice domain logica qrcode mokado ou na api do mercado pago

---
+++ fiap-fastfood/Adapters/Secondary/FastFood.Infra.Data/Migrations/20250526010321_SeedInitialProducts.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FastFood.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Produtos
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Category", "Price" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-1111-1111-111111111111"), "Hambúrguer Clássico", 0, 19.90m },
+                    { new Guid("22222222-2222-2222-2222-222222222222"), "Cheeseburger", 0, 21.90m },
+                    { new Guid("33333333-3333-3333-3333-333333333333"), "Batata Média", 1, 8.50m },
+                    { new Guid("44444444-4444-4444-4444-444444444444"), "Coca-Cola Lata", 2, 6.00m },
+                    { new Guid("55555555-5555-5555-5555-555555555555"), "Brownie com Sorvete", 3, 12.00m }
+                }
+            );
+
+            // Ingredientes
+            migrationBuilder.InsertData(
+                table: "CustomIngredients",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[,]
+                {
+                    { new Guid("aaaaaaa1-0000-0000-0000-000000000001"), "Pão", 0m },
+                    { new Guid("aaaaaaa2-0000-0000-0000-000000000002"), "Carne", 0m },
+                    { new Guid("aaaaaaa3-0000-0000-0000-000000000003"), "Alface", 0m },
+                    { new Guid("aaaaaaa4-0000-0000-0000-000000000004"), "Queijo", 0m },
+                    { new Guid("aaaaaaa5-0000-0000-0000-000000000005"), "Batata", 0m },
+                    { new Guid("aaaaaaa6-0000-0000-0000-000000000006"), "Refrigerante", 0m },
+                    { new Guid("aaaaaaa7-0000-0000-0000-000000000007"), "Brownie", 0m },
+                    { new Guid("aaaaaaa8-0000-0000-0000-000000000008"), "Sorvete de Baunilha", 0m }
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- fiap-fastfood/Adapters/Secondary/FastFood.Infra.Data/ProductRepository.cs
+++ fiap-fastfood/Adapters/Secondary/FastFood.Infra.Data/ProductRepository.cs
@@ -1,45 +1,26 @@
 ﻿using FastFood.Domain.Entities.OrderManagement;
 using FastFood.Domain.Entities.OrderManagement.ValueObjects;
 using FastFood.Domain.Ports.OrderManagement;
+using FastFood.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace FastFood.Infra.Data
 {
     public class ProductRepository : IProductRepository
     {
+        private readonly FastFoodDbContext _context;
+
+        public ProductRepository(FastFoodDbContext context)
+        {
+            _context = context;
+        }
+
+
         public IEnumerable<Product> ListAll()
         {
-            return new List<Product>
-            {
-                new Product(null, "X-Burger", EnumProductCategory.Meal, 18.50m,
-                    new List<CustomIngredient> {
-                        new CustomIngredient(null, "Pão", 200),
-                        new CustomIngredient(null, "Carne", 0),
-                        new CustomIngredient(null, "Queijo", 0)
-                    }, new ImageProduct("xburger.jpg", "https://cdn.exemplo.com/xburger.jpg")),
-
-                new Product(null, "X-Bacon", EnumProductCategory.Meal, 21.00m,
-                    new List<CustomIngredient> {
-                        new CustomIngredient(null, "Pão", 0),
-                        new CustomIngredient(null, "Bacon", 0),
-                        new CustomIngredient(null, "Queijo", 0)
-                    }, new ImageProduct("xbacon.jpg", "https://cdn.exemplo.com/xbacon.jpg")),
-
-                new Product(null, "Fritas Média", EnumProductCategory.SideDish, 9.90m,
-                    new List<CustomIngredient> {
-                        new CustomIngredient(null, "Batata", 0)
-                    }, null),
-
-                new Product(null, "Coca-Cola Lata", EnumProductCategory.Drink, 6.00m,
-                    new List<CustomIngredient> {
-                        new CustomIngredient(null, "Refrigerante", 0)
-                    }, null),
-
-                new Product(null, "Sorvete", EnumProductCategory.Dessert, 7.50m,
-                    new List<CustomIngredient> {
-                        new CustomIngredient(null, "Leite", 0),
-                        new CustomIngredient(null, "Açúcar", 0)
-                    }, new ImageProduct("sorvete.jpg", "https://cdn.exemplo.com/sorvete.jpg")),
-            };
+            return _context.Products
+                .Include(p => p.Ingredients)
+                .ToList();
         }
     }
 }
